7bf7bf37323bbc9aba37e3c0ab2e5da099103b45
domsurgeon <matias.alvarez@wunderman.com>
2019-07-19 21:08:06 -0300

    build

--- build/precache-manifest.75c550d10e9e18e5f28a6f652549244f.js
+++ build/precache-manifest.75c550d10e9e18e5f28a6f652549244f.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "0cf82dacf1271f78f0762c01868fab6b",
-    "url": "index.html"
+    "url": "/index.html"
   },
   {
     "revision": "ddbb9d59961893bcc3dd",
-    "url": "static/css/main.87643bed.chunk.css"
+    "url": "/static/css/main.87643bed.chunk.css"
   },
   {
     "revision": "9b848baaa737a08e222b",
-    "url": "static/js/2.34a44cb0.chunk.js"
+    "url": "/static/js/2.34a44cb0.chunk.js"
   },
   {
     "revision": "ddbb9d59961893bcc3dd",
-    "url": "static/js/main.ab8d5e4b.chunk.js"
+    "url": "/static/js/main.ab8d5e4b.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "static/js/runtime~main.a8a9905a.js"
+    "url": "/static/js/runtime~main.a8a9905a.js"
   }
 ]);
